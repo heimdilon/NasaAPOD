aeec52779754e701ae840c6fccaa4796d2f1f593
heimdilon <73837799+heimdilon@users.noreply.github.com>
2020-11-02 18:36:37 +0300

    Add files via upload

---
+++ Nasa_Api/NasaApi.py
@@ -0,0 +1,24 @@
+# Created by kor_a at 15 Ekim 2020
+# Date: 15.10.2020
+# Time: (23:25)
+from flask import Flask, render_template
+import requests
+
+#apod = requests.get("https://api.nasa.gov/planetary/apod?api_key=2T6sawFQJC8gfkmAI1GnGtWzu1tDqZguNnBwZRzR")
+#apod_json = apod.json()
+#image = apod_json['hdurl']
+
+app = Flask(__name__)
+@app.route("/")
+def get_image():
+    apod = requests.get("https://api.nasa.gov/planetary/apod?api_key=2T6sawFQJC8gfkmAI1GnGtWzu1tDqZguNnBwZRzR")
+    apod_json = apod.json()
+    image = apod_json['hdurl']
+    url = image
+    return render_template('index.html', url=url)
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run()
